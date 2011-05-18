59213afd361fa0cb2b5d1729fb93546ba3ebef15
Zachary Waldowski <zwaldowski@gmail.com>
2011-05-17 23:53:16 -0400

    Implement UIBarButtonItem extensions.

--- BlocksKit/UIBarButtonItem+BlocksKit.h
+++ BlocksKit/UIBarButtonItem+BlocksKit.h
@@ -3,6 +3,46 @@
 //  BlocksKit
 //
 
+/** Block event initialization for UIBarButtonItem.
+
+ This set of extensions has near-drop-in replacements
+ for the standard set of UIBarButton item initializations,
+ using a block handler instead of a target/selector.
+
+ Includes code by the following:
+
+ - Kevin O'Neill.  <https://github.com/kevinoneill>. 2011. BSD.
+ - Zach Waldowski. <https://github.com/zwaldowski>.  2011. MIT.
+ */
 @interface UIBarButtonItem (BlocksKit)
+
+/** Creates and returns a configured item containing the specified system item.
+
+ @return Newly initialized item with the specified properties.
+ @param systemItem The system item to use as the item representation. One of the constants defined in UIBarButtonSystemItem.
+ @param handler The block that gets fired on the button press.
+ */
+- (id)initWithBarButtonSystemItem:(UIBarButtonSystemItem)systemItem handler:(void (^)(id sender))action;
+
+/** Creates and returns a configured item using the specified image and style.
+ 
+ @return Newly initialized item with the specified properties.
+ @param image.  The item’s image. If nil an image is not displayed.
+ If this image is too large to fit on the bar, it is scaled to fit
+ The size of a toolbar and navigation bar image is 20 x 20 points.
+ @param style The style of the item. One of the constants defined in UIBarButtonItemStyle.
+ @param handler The block that gets fired on the button press.
+ */
+- (id)initWithImage:(UIImage *)image style:(UIBarButtonItemStyle)style handler:(void (^)(id sender))action;
+
+/** Creates and returns a configured item using the specified text and style.
+ 
+ @return Newly initialized item with the specified properties.
+ @param title The text displayed on the button item.
+ @param style The style of the item. One of the constants defined in UIBarButtonItemStyle.
+ @param handler The block that gets fired on the button press.
+ */
+- (id)initWithTitle:(NSString *)title style:(UIBarButtonItemStyle)style handler:(void (^)(id sender))action;
+
 
 @end
--- BlocksKit/UIBarButtonItem+BlocksKit.m
+++ BlocksKit/UIBarButtonItem+BlocksKit.m
@@ -4,7 +4,41 @@
 //
 
 #import "UIBarButtonItem+BlocksKit.h"
+#import "NSObject+AssociatedObjects.h"
+
+static char kBarButtonItemBlockKey; 
+
+@interface UIBarButtonItem (BlocksKitPrivate)
+- (void)_handleAction:(UIBarButtonItem *)sender;
+@end
 
 @implementation UIBarButtonItem (BlocksKit)
+
+- (id)initWithBarButtonSystemItem:(UIBarButtonSystemItem)systemItem handler:(void (^)(id sender))action {
+    if ((self = [self initWithBarButtonSystemItem:systemItem target:self action:@selector(_handleAction:)])) {
+        [self associateCopyOfValue:action withKey:&kBarButtonItemBlockKey];
+    }
+    return self;
+}
+
+- (id)initWithImage:(UIImage *)image style:(UIBarButtonItemStyle)style handler:(void (^)(id sender))action {
+    if ((self = [self initWithImage:image style:style target:self action:@selector(_handleAction:)])) {
+        [self associateCopyOfValue:action withKey:&kBarButtonItemBlockKey];
+    }
+    return self;
+}
+
+- (id)initWithTitle:(NSString *)title style:(UIBarButtonItemStyle)style handler:(void (^)(id sender))action {
+    if ((self = [self initWithTitle:title style:style target:self action:@selector(_handleAction:)])) {
+        [self associateCopyOfValue:action withKey:&kBarButtonItemBlockKey];
+    }
+    return self;
+}
+
+- (void)_handleAction:(UIBarButtonItem *)sender {
+    __block void (^handler)(id sender) = [self associatedValueForKey:&kBarButtonItemBlockKey];
+    if (handler)
+        handler(sender);
+}
 
 @end
