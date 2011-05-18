427e498ed595aa151421367e077b7c08a7c53854
Zachary Waldowski <zwaldowski@gmail.com>
2011-05-17 23:17:56 -0400

    Append @rentzsch's NSInvocation superheroics.

---
+++ BlocksKit/NSInvocation+BlocksKit.h
@@ -0,0 +1,26 @@
+//
+//  NSInvocation+BlocksKit.h
+//  BlocksKit
+//
+
+/** Blocks wrapper for NSInvocation.
+
+ Usage example:
+     NSInvocation *invocation = [NSInvocation jr_invocationWithTarget:myObject block:^(id myObject){
+       [myObject someMethodWithArg:42.0];
+     }];
+ 
+ Created by Jonathan Rentzch as [NSInvocation-blocks](https://github.com/rentzsch/NSInvocation-blocks).
+ Licensed under MIT.
+ */
+@interface NSInvocation (BlocksKit)
+
+/** Generates an `NSInvocation` object for a given block.
+
+ @param target The object to "grab" the block invocation from.
+ @param block A code block.
+ @return A fully-prepared instance of NSInvocation ready to be invoked
+ */
++ (NSInvocation *)invocationWithTarget:(id)target block:(void (^)(id target))block;
+
+@end
---
+++ BlocksKit/NSInvocation+BlocksKit.m
@@ -0,0 +1,51 @@
+//
+//  NSInvocation+BlocksKit.m
+//  BlocksKit
+//
+
+#import "NSInvocation+BlocksKit.h"
+
+@interface JRInvocationGrabber : NSProxy {
+    id              target;
+    NSInvocation    *invocation;
+}
+@property (retain) id target;
+@property (retain) NSInvocation *invocation;
+
++ (JRInvocationGrabber *)grabberWithTarget:(id)target;
+@end
+
+@implementation JRInvocationGrabber
+@synthesize target, invocation;
+
++ (JRInvocationGrabber *)grabberWithTarget:(id)target {
+    JRInvocationGrabber *instance = [[JRInvocationGrabber alloc] init];
+    instance.target = target;
+    return [instance autorelease];
+}
+
+- (NSMethodSignature*)methodSignatureForSelector:(SEL)selector_ {
+    return [self.target methodSignatureForSelector:selector_];
+}
+
+- (void)forwardInvocation:(NSInvocation*)invocation_ {
+    [invocation_ setTarget:self.target];
+    self.invocation = invocation_;
+}
+
+- (void)dealloc {
+    self.target = nil;
+    self.invocation = nil;
+    [super dealloc];
+}
+@end
+
+@implementation NSInvocation (BlocksKit)
+
++ (NSInvocation *)invocationWithTarget:(id)target block:(void (^)(id target))block {
+    JRInvocationGrabber *grabber = [JRInvocationGrabber grabberWithTarget:target];
+    block(grabber);
+    return grabber.invocation;
+}
+
+@end
