7596bb2fb709f75ff3076fe57b03d86d904ff122
Zachary Waldowski <zwaldowski@gmail.com>
2011-05-18 00:05:41 -0400

    UIGestureRecognizer support.

--- BlocksKit/UIGestureRecognizer+BlocksKit.h
+++ BlocksKit/UIGestureRecognizer+BlocksKit.h
@@ -3,6 +3,33 @@
 //  BlocksKit
 //
 
+/** Block initialization for UIGestureRecognizer.
+ 
+ Includes code by the following:
+ 
+ - Kevin O'Neill.  <https://github.com/kevinoneill>. 2011. BSD.
+ - Zach Waldowski. <https://github.com/zwaldowski>.  2011. MIT.
+ */
 @interface UIGestureRecognizer (BlocksKit)
+
+/** An autoreleased gesture recognizer that will call the given block.
+  
+ @return An initialized and autoreleased instance of a concrete UIGestureRecognizer subclass, or `nil`.
+ @param handler The block which handles an executed gesture.
+ @see initWithTarget:action:
+ @see addHandler:
+ */
++ (id)recognizerWithHandler:(void (^)(id recognizer))block;
+
+/** Initializes an allocated gesture recognizer that will call the given block.
+
+ An alternative to the designated initializer.
+
+ @return An initialized instance of a concrete UIGestureRecognizer subclass or `nil`.
+ @param handler The block which handles an executed gesture.
+ @see initWithTarget:action:
+ @see addHandler:
+ */
+- (id)initWithHandler:(void (^)(id recognizer))block;
 
 @end
--- BlocksKit/UIGestureRecognizer+BlocksKit.m
+++ BlocksKit/UIGestureRecognizer+BlocksKit.m
@@ -4,7 +4,31 @@
 //
 
 #import "UIGestureRecognizer+BlocksKit.h"
+#import "NSObject+AssociatedObjects.h"
+
+static char kGestureRecognizerBlockKey;
+
+@interface UIGestureRecognizer (BlocksKitInternal)
+- (void)_handleAction:(id)recognizer;
+@end
 
 @implementation UIGestureRecognizer (BlocksKit)
+
++ (id)recognizerWithHandler:(void (^)(id recognizer))block {
+    return [[[[self class] alloc] initWithHandler:block] autorelease];
+}
+
+- (id)initWithHandler:(void (^)(id recognizer))block {
+    if ((self = [self initWithTarget:self action:@selector(_handleAction:)])) {
+        [self associateCopyOfValue:block withKey:&kGestureRecognizerBlockKey];
+    }
+    return self;
+}
+                 
+- (void)_handleAction:(id)recognizer {
+    void (^handler)(id recognizer) = [self associatedValueForKey:&kGestureRecognizerBlockKey];
+    if (handler)
+        handler(recognizer);
+}
 
 @end
