914163505a152c1ab4da77131fda960c48cd4b62
Zachary Waldowski <zwaldowski@gmail.com>
2011-05-17 23:38:57 -0400

    Complete the UIControl additions.

--- BlocksKit/UIControl+BlocksKit.h
+++ BlocksKit/UIControl+BlocksKit.h
@@ -3,6 +3,37 @@
 //  BlocksKit
 //
 
+/** Block control event handling for UIControl.
+ 
+ This set of extensions allows for two pathways of block
+ handling for a UIControl instance.  There's the basic
+ setup, which adds a block 
+ 
+ Includes code by the following:
+ 
+ - Kevin O'Neill.  <https://github.com/kevinoneill>. 2011. BSD.
+ - Zach Waldowski. <https://github.com/zwaldowski>.  2011. MIT.
+ */
 @interface UIControl (BlocksKit)
+
+///-----------------------------------
+/// @name Block event handling
+///-----------------------------------
+
+/** Adds a block for a particular event to an internal dispatch table.
+
+ @param handler A block representing an action message, with an argument for the sender.
+ @param controlEvents A bitmask specifying the control events for which the action message is sent.
+ @see addTarget:action:forControlEvents:
+ @see removeEventHandlersForControlEvents:
+ */
+- (void)addEventHandler:(void (^)(id sender))handler forControlEvents:(UIControlEvents)controlEvents;
+
+/** Removes all blocks for a particular event combination.
+ @param controlEvents A bitmask specifying the control events for which the block will be removed.
+ @see addTarget:action:forControlEvents:
+ @see addEventHandler:forControlEvents:
+ */
+- (void)removeEventHandlersForControlEvents:(UIControlEvents)controlEvents;
 
 @end
--- BlocksKit/UIControl+BlocksKit.m
+++ BlocksKit/UIControl+BlocksKit.m
@@ -4,7 +4,77 @@
 //
 
 #import "UIControl+BlocksKit.h"
+#import "NSObject+AssociatedObjects.h"
+#import "NSArray+BlocksKit.h"
+
+static char kControlBlockArrayKey; 
+
+@interface BKBlockWrapper : NSObject {
+@private
+    void (^action)(id sender);
+    UIControlEvents controlEvents;
+}
+
++ (BKBlockWrapper *)wrapperWithAction:(void (^)(id sender))action forControlEvents:(UIControlEvents)controlEvents;
+
+@property (nonatomic, copy) void (^action)(id sender);
+@property (nonatomic, assign) UIControlEvents controlEvents;
+
+- (void)invokeWithSender:(id)sender;
+
+@end
+
+@implementation BKBlockWrapper
+
+@synthesize action, controlEvents;
+
++ (BKBlockWrapper *)wrapperWithAction:(void (^)(id sender))action forControlEvents:(UIControlEvents)controlEvents {
+    BKBlockWrapper *instance = [[BKBlockWrapper alloc] init];
+    instance.action = action;
+    instance.controlEvents = controlEvents;
+    return [instance autorelease];
+}
+
+- (void)dealloc {
+    [action release]; action = nil;
+    [super dealloc];
+}
+
+- (void)invokeWithSender:(id)sender {
+    self.action(sender);
+}
+
+@end
+
 
 @implementation UIControl (BlocksKit)
+
+- (void)addEventHandler:(void (^)(id sender))handler forControlEvents:(UIControlEvents)controlEvents {
+    NSMutableArray *actions = [self associatedValueForKey:&kControlBlockArrayKey];
+    
+    if (!actions) {
+        actions = [[NSMutableArray alloc] init];
+        [self associateValue:actions withKey:&kControlBlockArrayKey];
+        [actions release];
+    }
+    
+    BKBlockWrapper *target = [BKBlockWrapper wrapperWithAction:handler forControlEvents:controlEvents];
+    [actions addObject:target];
+    [self addTarget:target action:@selector(invokeWithSender:) forControlEvents:controlEvents];
+    [target release];    
+}
+
+- (void)removeEventHandlersForControlEvents:(UIControlEvents)controlEvents {
+    NSMutableArray *actions = [self associatedValueForKey:&kControlBlockArrayKey];
+    
+    if (!actions)
+        return;
+    
+    [actions removeObjectsInArray:[actions select:^BOOL(id obj) {
+        if ([obj isKindOfClass:[BKBlockWrapper class]])
+            return ([(BKBlockWrapper*)obj controlEvents] == controlEvents);
+        return NO;
+    }]];
+}
 
 @end
