d2416c5c4baf74d8af29c9d571375fbd65d90a04
Zachary Waldowski <zwaldowski@gmail.com>
2011-05-18 00:20:03 -0400

    Add the UIView convenience methods.

--- BlocksKit/UIView+BlocksKit.h
+++ BlocksKit/UIView+BlocksKit.h
@@ -3,6 +3,65 @@
 //  BlocksKit
 //
 
+/** Convenience on-touch methods for UIView.
+
+ Includes code by the following:
+
+ - Kevin O'Neill.  <https://github.com/kevinoneill>. 2011. BSD.
+ - Jake Marsh.     <https://github.com/jakemarsh>.   2011. 
+ - Zach Waldowski. <https://github.com/zwaldowski>.  2011. MIT.
+ */
 @interface UIView (BlocksKit)
+
+/** Abstract creation of a block-backed UITapGestureRecognizer.
+
+ This method allows for the recognition of any arbitrary number
+ of fingers tapping any number of times on a view.  An instance
+ of UITapGesture recognizer is allocated for the block and added
+ to the recieving view.
+
+ @param numberOfTouches The number of fingers tapping that will trigger the block.
+ @param numberOfTaps The number of taps required to trigger the block.
+ @param block The handler for the UITapGestureRecognizer
+ @see whenTapped:
+ @see whenDoubleTapped:
+ */
+- (void)whenTouches:(NSUInteger)numberOfTouches tapped:(NSUInteger)numberOfTaps handler:(void (^)())block;
+
+/** Adds a recognizer for one finger tapping once.
+
+ @param block The handler for the tap recognizer
+ @see whenDoubleTapped:
+ @see whenTouches:tapped:handler:
+ */
+- (void)whenTapped:(void (^)())block;
+
+/** Adds a recognizer for one finger tapping twice.
+ 
+ @param block The handler for the tap recognizer
+ @see whenTapped:
+ @see whenTouches:tapped:handler:
+ */
+- (void)whenDoubleTapped:(void (^)())block;
+
+/** Adds a block that gets called on a finger down.
+
+ Internally, this method overrides the touchesBegan:withEvent:
+ selector of UIView and is mechanically similar to
+ UIControlEventTouchDown.
+
+ @param block The handler for the touch recognizer
+ */
+- (void)whenTouchedDown:(void (^)())block;
+
+/** Adds a block that gets called on a finger up.
+ 
+ Internally, this method overrides the touchesBegan:withEvent:
+ selector of UIView and is mechanically similar to
+ UIControlEventTouchCancel.
+ 
+ @param block The handler for the touch recognizer
+ */
+- (void)whenTouchedUp:(void (^)())block;
 
 @end
--- BlocksKit/UIView+BlocksKit.m
+++ BlocksKit/UIView+BlocksKit.m
@@ -7,7 +7,68 @@
 //
 
 #import "UIView+BlocksKit.h"
+#import "NSObject+AssociatedObjects.h"
+#import "UIGestureRecognizer+BlocksKit.h"
+#import "NSArray+BlocksKit.h"
+
+static char kViewTouchDownBlockKey;
+static char kViewTouchUpBlockKey;
 
 @implementation UIView (BlocksKit)
+
+- (void)whenTouches:(NSUInteger)numberOfTouches tapped:(NSUInteger)numberOfTaps handler:(void (^)())block {
+    self.userInteractionEnabled = YES;
+    
+    UITapGestureRecognizer *gesture = [[UITapGestureRecognizer alloc] initWithHandler:^(id recognizer) {
+        block();
+    }];
+    
+    [[self.gestureRecognizers select:^BOOL(id obj) {
+        if ([obj isKindOfClass:[UITapGestureRecognizer class]]) {
+            BOOL rightTouches = ([(UITapGestureRecognizer *)obj numberOfTouchesRequired] == numberOfTouches);
+            BOOL rightTaps = ([(UITapGestureRecognizer *)obj numberOfTapsRequired] == numberOfTaps);
+            return (rightTouches && rightTaps);
+        }
+        return NO;
+    }] each:^(id obj) {
+        [gesture requireGestureRecognizerToFail:(UITapGestureRecognizer *)obj];
+    }];
+    
+    [gesture setNumberOfTouchesRequired:numberOfTouches];
+    [gesture setNumberOfTapsRequired:numberOfTaps];
+    
+    [self addGestureRecognizer:gesture];
+    [gesture release];    
+}
+
+- (void)whenTapped:(void (^)())block {
+    [self whenTouches:1 tapped:1 handler:block];
+}
+
+- (void)whenDoubleTapped:(void (^)())block {
+    [self whenTouches:2 tapped:1 handler:block];
+}
+
+- (void)whenTouchedDown:(void (^)())block {
+    self.userInteractionEnabled = YES;
+    [self associateCopyOfValue:block withKey:&kViewTouchDownBlockKey];
+}
+
+- (void)whenTouchedUp:(void (^)())block {
+    self.userInteractionEnabled = YES;
+    [self associateCopyOfValue:block withKey:&kViewTouchUpBlockKey];
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    [super touchesBegan:touches withEvent:event];
+    void (^block)() = [self associatedValueForKey:&kViewTouchDownBlockKey];
+    if (block) block();
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+    [super touchesEnded:touches withEvent:event];
+    void (^block)() = [self associatedValueForKey:&kViewTouchUpBlockKey];
+    if (block) block();
+}
 
 @end
