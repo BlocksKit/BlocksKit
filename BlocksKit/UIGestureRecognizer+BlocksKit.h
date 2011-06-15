44aada7ab6d9fed5f3ca9c8e593ef210ca4c0158
Zachary Waldowski <zwaldowski@gmail.com>
2011-06-15 02:02:36 -0400

    Support delays on UIGestureRecognizer, with built-in cancellation.  Allows for a sketchy, but speedier version of requireGestureRecognizerToFail:.

--- BlocksKit/UIGestureRecognizer+BlocksKit.h
+++ BlocksKit/UIGestureRecognizer+BlocksKit.h
@@ -17,16 +17,40 @@
 
 @interface UIGestureRecognizer (BlocksKit)
 
+/** An autoreleased gesture recognizer that will, on firing, call
+ the given block asynchronously after a number of seconds.
+
+ @return An autoreleased instance of a concrete UIGestureRecognizer subclass, or `nil`.
+ @param block The block which handles an executed gesture.
+ @param delay A number of seconds after which the block will fire.
+ */
++ (id)recognizerWithHandler:(BKSenderBlock)block delay:(NSTimeInterval)delay;
+
+/** Initializes an allocated gesture recognizer that will call the given block
+ after a given delay.
+ 
+ An alternative to the designated initializer.
+ 
+ @return An initialized instance of a concrete UIGestureRecognizer subclass or `nil`.
+ @param block The block which handles an executed gesture.
+ @param delay A number of seconds after which the block will fire.
+ */
+- (id)initWithHandler:(BKSenderBlock)block delay:(NSTimeInterval)delay;
+
 /** An autoreleased gesture recognizer that will call the given block.
+ 
+ For convenience and compatibility reasons, this method is indentical
+ to using recognizerWithHandler:delay: with a delay of 0.0.
   
- @return An initialized and autoreleased instance of a concrete UIGestureRecognizer subclass, or `nil`.
+ @return An initialized and autoreleased instance of a concrete UIGestureRecognizer
+ subclass, or `nil`.
  @param block The block which handles an executed gesture.
  */
 + (id)recognizerWithHandler:(BKSenderBlock)block;
 
 /** Initializes an allocated gesture recognizer that will call the given block.
-
- An alternative to the designated initializer.
+ 
+ This method is indentical to calling initWithHandler:delay: with a delay of 0.0.
 
  @return An initialized instance of a concrete UIGestureRecognizer subclass or `nil`.
  @param block The block which handles an executed gesture.
@@ -37,5 +61,15 @@
  to be modified after the fact.
  */
 @property (copy) BKSenderBlock handler;
+
+/** Allows the length of the delay after which the gesture
+ recognizer will be fired to modify.
+ */
+@property (assign) NSTimeInterval delay;
+
+/** If the recognizer happens to be fired, calling this method
+ will stop it from firing, but only if a delay is set.
+ */
+- (void)cancel;
 
 @end
--- BlocksKit/UIGestureRecognizer+BlocksKit.m
+++ BlocksKit/UIGestureRecognizer+BlocksKit.m
@@ -5,29 +5,50 @@
 
 #import "UIGestureRecognizer+BlocksKit.h"
 #import "NSObject+AssociatedObjects.h"
+#import "NSObject+BlocksKit.h"
 
 static char *kGestureRecognizerBlockKey = "BKGestureRecognizerBlock";
+static char *kGestureRecognizerDelayKey = "BKGestureRecognizerDelay";
+static char *kGestureRecognizerCancelRefKey = "BKGestureRecognizerCancellationBlock";
 
 @interface UIGestureRecognizer (BlocksKitInternal)
 - (void)_handleAction:(id)recognizer;
+- (void)_handleActionUsingDelay:(id)recognizer;
 @end
 
 @implementation UIGestureRecognizer (BlocksKit)
 
++ (id)recognizerWithHandler:(BKSenderBlock)block delay:(NSTimeInterval)delay {
+    return [[[[self class] alloc] initWithHandler:block delay:delay] autorelease];
+}
+
+- (id)initWithHandler:(BKSenderBlock)block delay:(NSTimeInterval)delay {
+    self = [self init];
+    [self setHandler:block];
+    [self setDelay:delay];
+    return self;
+}
+
 + (id)recognizerWithHandler:(BKSenderBlock)block {
-    return [[[[self class] alloc] initWithHandler:block] autorelease];
+    return [self recognizerWithHandler:block delay:0.0];
 }
 
 - (id)initWithHandler:(BKSenderBlock)block {
-    self = [self initWithTarget:self action:@selector(_handleAction:)];
-    [self setHandler:block];
-    return self;
+    return [self initWithHandler:block delay:0.0];
 }
-                 
+
 - (void)_handleAction:(id)recognizer {
     BKSenderBlock block = self.handler;
     if (block)
         block(self);
+}
+
+- (void)_handleActionUsingDelay:(id)recognizer {
+    BKSenderBlock block = self.handler;
+    if (block) {
+        id cancel = [NSObject performBlock:^{ block(self); } afterDelay:self.delay];
+        [self weaklyAssociateValue:cancel withKey:kGestureRecognizerCancelRefKey];
+    }
 }
 
 - (void)setHandler:(BKSenderBlock)handler {
@@ -36,6 +57,29 @@ static char *kGestureRecognizerBlockKey = "BKGestureRecognizerBlock";
 
 - (BKSenderBlock)handler {
     return [self associatedValueForKey:kGestureRecognizerBlockKey];
+}
+
+- (void)setDelay:(NSTimeInterval)delay {
+    [self removeTarget:self action:NULL];
+    if (delay)
+        [self addTarget:self action:@selector(_handleActionUsingDelay:)];
+    else
+        [self addTarget:self action:@selector(_handleAction:)];
+    [self associateValue:[NSNumber numberWithDouble:(delay) ? delay : 0.0] withKey:kGestureRecognizerDelayKey];
+}
+
+- (NSTimeInterval)delay {
+    NSNumber *delay = [self associatedValueForKey:kGestureRecognizerDelayKey];
+    if (delay)
+        return [delay doubleValue];
+    else
+        return 0.0;
+}
+
+- (void)cancel {
+    id cancel = [self associatedValueForKey:kGestureRecognizerCancelRefKey];
+    if (cancel)
+        [NSObject cancelBlock:cancel];
 }
 
 @end
