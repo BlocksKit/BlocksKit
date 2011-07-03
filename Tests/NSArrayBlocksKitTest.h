a778156eb48be4d9adc12606f5197875089b3059
Kai Wu <kaiwu2004@gmail.com>
2011-07-03 16:24:40 +0800

    the unittest for NSArray blocks

---
+++ Tests/NSArrayBlocksKitTest.h
@@ -0,0 +1,17 @@
+//
+//  NSArrayBlocksKitTest.h
+//  BlocksKit
+//
+//  Created by WU Kai on 7/3/11.
+//  Copyright 2011 Dizzy Technology. All rights reserved.
+//
+
+#import <GHUnitIOS/GHUnit.h>
+#import "BlocksKit/BlocksKit.h"
+
+@interface NSArrayBlocksKitTest : GHTestCase {
+    NSArray *_subject;
+    NSInteger _total;
+}
+
+@end
---
+++ Tests/TestsAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  TestsAppDelegate.h
+//  Tests
+//
+//  Created by WU Kai on 7/3/11.
+//  Copyright 2011 None. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestsAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
