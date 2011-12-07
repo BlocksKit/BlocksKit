b8b315a6d439d87bd2ad209828b6c3d96ad00d2d
Alexsander Akers <a2@pandamonia.us>
2011-12-07 18:45:22 -0500

    Write better `property_copyAttributeValue` wrapper

--- A2BlockDelegate.m
+++ A2BlockDelegate.m
@@ -160,7 +160,7 @@ static void a2_blockPropertySetter(id self, SEL _cmd, id block);
 	}];
 	
 	return found;
-}
+;}
 
 + (NSDictionary *) mapForProtocol: (Protocol *) protocol
 {
@@ -277,34 +277,6 @@ static void a2_blockPropertySetter(id self, SEL _cmd, id block);
 
 @end
 
-static char *a2_property_copyAttributeValue(objc_property_t property, const char *attributeName)
-{
-	if (&property_copyAttributeValue)
-	{
-		return property_copyAttributeValue(property, attributeName);
-	}
-	
-	const unsigned long attributeNameLength = strlen(attributeName);
-	const char *attributes = property_getAttributes(property);
-	
-	char buffer[strlen(attributes) + 1];
-	strcpy(buffer, attributes);
-	
-	char *state = buffer, *attribute;
-	
-	while ((attribute = strsep(&state, ",")))
-	{
-		if (!strncmp(attribute, attributeName, attributeNameLength))
-		{
-			break;
-		}
-	}
-	
-	const unsigned long attributeLength = strlen(attribute) - attributeLength;
-	char *returnBuffer = malloc(attributeLength);
-	strcpy(returnBuffer, attribute + attributeNameLength);
-	return returnBuffer;
-}
 static void *a2_blockPropertyGetter(NSObject *self, SEL _cmd)
 {
 	Protocol *protocol;
@@ -322,4 +294,133 @@ static void a2_blockPropertySetter(NSObject *self, SEL _cmd, id block)
 		return;
 	
 	[[self dynamicDelegateForProtocol: protocol] implementMethod: representedSelector withBlock: block];
+}
+
+/*
+ * Copyright (c) 1999-2007 Apple Inc.  All Rights Reserved.
+ * 
+ * @APPLE_LICENSE_HEADER_START@
+ * 
+ * This file contains Original Code and/or Modifications of Original Code
+ * as defined in and that are subject to the Apple Public Source License
+ * Version 2.0 (the 'License'). You may not use this file except in
+ * compliance with the License. Please obtain a copy of the License at
+ * http://www.opensource.apple.com/apsl/ and read it before using this
+ * file.
+ * 
+ * The Original Code and all software distributed under the License are
+ * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
+ * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
+ * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
+ * Please see the License for the specific language governing rights and
+ * limitations under the License.
+ * 
+ * @APPLE_LICENSE_HEADER_END@
+ */
+
+/*
+ * Changelog:
+ * - 12/7/11 - Wrote wrapper for `property_copyAttributeValue` if it is not defined (Alexsander Akers)
+ */
+
+static unsigned int a2_iteratePropertyAttributes(const char *attrs, BOOL (*fn)(unsigned int index, void *ctx1, void *ctx2, const char *name, size_t nlen, const char *value, size_t vlen), void *ctx1, void *ctx2)
+{
+	/*
+	 Property attribute string format:
+	 - Comma-separated name-value pairs. 
+	 - Name and value may not contain ,
+	 - Name may not contain "
+	 - Value may be empty
+	 - Name is single char, value follows
+	 - OR Name is double-quoted string of 2+ chars, value follows
+	 */
+	
+    if (!attrs) return 0;
+	
+    const char *attrsend = attrs + strlen(attrs);
+    unsigned int attrcount = 0;
+	
+    while (*attrs) {
+        // Find the next comma-separated attribute
+        const char *start = attrs;
+        const char *end = start + strcspn(attrs, ",");
+		
+        // Move attrs past this attribute and the comma (if any)
+        attrs = *end ? end+1 : end;
+		
+        assert(attrs <= attrsend);
+        assert(start <= attrsend);
+        assert(end <= attrsend);
+        
+        // Skip empty attribute
+        if (start == end) continue;
+		
+        // Process one non-empty comma-free attribute [start,end)
+        const char *nameStart;
+        const char *nameEnd;
+		
+        assert(start < end);
+        assert(*start);
+        if (*start != '\"') {
+            // single-char short name
+            nameStart = start;
+            nameEnd = start+1;
+            start++;
+        }
+        else {
+            // double-quoted long name
+            nameStart = start+1;
+            nameEnd = nameStart + strcspn(nameStart, "\",");
+            start++;                       // leading quote
+            start += nameEnd - nameStart;  // name
+            if (*start == '\"') start++;   // trailing quote, if any
+        }
+		
+        // Process one possibly-empty comma-free attribute value [start,end)
+        const char *valueStart;
+        const char *valueEnd;
+		
+        assert(start <= end);
+		
+        valueStart = start;
+        valueEnd = end;
+		
+        BOOL more = (*fn)(attrcount, ctx1, ctx2, 
+                          nameStart, nameEnd-nameStart, 
+                          valueStart, valueEnd-valueStart);
+        attrcount++;
+        if (!more) break;
+    }
+	
+    return attrcount;
+}
+static BOOL a2_findOneAttribute(unsigned int index, void *ctxa, void *ctxs, const char *name, size_t nlen, const char *value, size_t vlen)
+{
+	const char *query = (char *)ctxa;
+    char **resultp = (char **)ctxs;
+	
+    if (strlen(query) == nlen  &&  0 == strncmp(name, query, nlen)) {
+        char *result = calloc(vlen+1, 1);
+        memcpy(result, value, vlen);
+        result[vlen] = '\0';
+        *resultp = result;
+        return NO;
+    }
+	
+    return YES;
+}
+static char *a2_property_copyAttributeValue(objc_property_t property, const char *name)
+{
+	if (&property_copyAttributeValue)
+	{
+		return property_copyAttributeValue(property, name);
+	}
+	
+	if (!property || !name || *name == '\0') return NULL;
+	
+    char *result = NULL;
+	const char *attributes = property_getAttributes(property);
+	a2_iteratePropertyAttributes(attributes, &a2_findOneAttribute, (void *) name, &result);
+    return result;
 }
