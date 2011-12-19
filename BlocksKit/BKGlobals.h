0e3a831688dd8d4fb196876c46e936c2555d541e
Alexsander Akers <a2@pandamonia.us>
2011-12-19 13:54:39 -0500

    Update `BKGlobals.h` to be more precise
    Signed-off-by: Alexsander Akers <a2@pandamonia.us>

--- BlocksKit/BKGlobals.h
+++ BlocksKit/BKGlobals.h
@@ -5,24 +5,23 @@
 
 #pragma once
 
-#import <Foundation/Foundation.h>
 #import <dispatch/dispatch.h>
+#import <Foundation/Foundation.h>
+
 #import "A2BlockDelegate.h"
 #import "A2DynamicDelegate.h"
 
-#if TARGET_IPHONE_SIMULATOR
+#ifdef __IPHONE_OS_VERSION_MIN_REQUIRED
     #define BK_HAS_UIKIT 1
     #define BK_HAS_APPKIT 0
-#elif TARGET_OS_IPHONE
-    #define BK_HAS_UIKIT 1
-    #define BK_HAS_APPKIT 0
-#else
+#elif defined(__MAC_OS_X_VERSION_MIN_REQUIRED)
+	#define BK_HAS_APPKIT 1
+
     #if CHAMELEON
         #define BK_HAS_UIKIT 1
     #else
         #define BK_HAS_UIKIT 0
     #endif
-#define BK_HAS_APPKIT 1
 #endif
 
 #if BK_HAS_APPKIT
@@ -33,46 +32,47 @@
 #import <UIKit/UIKit.h>
 #import <MessageUI/MessageUI.h>
 
-typedef void(^BKGestureRecognizerBlock)(UIGestureRecognizer *sender, UIGestureRecognizerState state, CGPoint location);
-typedef void(^BKTouchBlock)(NSSet* set, UIEvent* event);
+typedef void (^BKGestureRecognizerBlock)(UIGestureRecognizer *sender, UIGestureRecognizerState state, CGPoint location);
+typedef void (^BKTouchBlock)(NSSet* set, UIEvent* event);
+
 #endif
 
-typedef void(^BKBlock)(void); // compatible with dispatch_block_t
-typedef void(^BKSenderBlock)(id sender);
-typedef void(^BKIndexBlock)(NSUInteger index);
-typedef void(^BKTimerBlock)(NSTimeInterval time);
-typedef void(^BKResponseBlock)(NSURLResponse *response);
+typedef void (^BKBlock)(void); // compatible with dispatch_block_t
+typedef void (^BKSenderBlock)(id sender);
+typedef void (^BKIndexBlock)(NSUInteger index);
+typedef void (^BKTimerBlock)(NSTimeInterval time);
+typedef void (^BKResponseBlock)(NSURLResponse *response);
 
-typedef void(^BKObservationBlock)(id obj, NSDictionary *change);
-typedef void(^BKKeyValueBlock)(id key, id obj);
+typedef void (^BKObservationBlock)(id obj, NSDictionary *change);
+typedef void (^BKKeyValueBlock)(id key, id obj);
 
-typedef BOOL(^BKValidationBlock)(id obj);
-typedef BOOL(^BKKeyValueValidationBlock)(id key, id obj);
-typedef BOOL(^BKIndexValidationBlock)(NSUInteger index);
+typedef BOOL (^BKValidationBlock)(id obj);
+typedef BOOL (^BKKeyValueValidationBlock)(id key, id obj);
+typedef BOOL (^BKIndexValidationBlock)(NSUInteger index);
 
-typedef id(^BKReturnBlock)(void);
-typedef id(^BKTransformBlock)(id obj);
-typedef id(^BKKeyValueTransformBlock)(id key, id obj);
-typedef id(^BKAccumulationBlock)(id sum, id obj);
+typedef id (^BKReturnBlock)(void);
+typedef id (^BKTransformBlock)(id obj);
+typedef id (^BKKeyValueTransformBlock)(id key, id obj);
+typedef id (^BKAccumulationBlock)(id sum, id obj);
 
-typedef NSUInteger(^BKIndexTransformBlock)(NSUInteger index);
+typedef NSUInteger (^BKIndexTransformBlock)(NSUInteger index);
 
 #ifndef __has_feature
-#define __has_feature(x) 0
+	#define __has_feature(x) 0
 #endif
 
 #if __has_feature(objc_arc)
-#define BK_AUTORELEASE(o) o
-#define BK_SHOULD_DEALLOC 0
-#define BK_RELEASE(o)
-#define BK_SET_RETAINED(var, val) var = val
+	#define BK_AUTORELEASE(o) o
+	#define BK_SHOULD_DEALLOC 0
+	#define BK_RELEASE(o)
+	#define BK_SET_RETAINED(var, val) var = val
 #else
-#define BK_AUTORELEASE(o) [o autorelease]
-#define BK_SHOULD_DEALLOC 1
-#define BK_RELEASE(o) [o release]
-#define BK_SET_RETAINED(var, val) { \
-if (var) \
-[var release]; \
-var = [val retain]; \
-}
+	#define BK_AUTORELEASE(o) [o autorelease]
+	#define BK_SHOULD_DEALLOC 1
+	#define BK_RELEASE(o) [o release]
+	#define BK_SET_RETAINED(var, val) \
+		do { \
+			[var autorelease]; \
+			var = [val retain]; \
+		} while (0)
 #endif
