a59f737e1d87d03552cd5c914a76ecc2f293c64c
Zachary Waldowski <zwaldowski@gmail.com>
2011-12-20 22:43:17 -0500

    UIAlertView unit tests.
    Signed-off-by: Zachary Waldowski <zwaldowski@gmail.com>

---
+++ Tests/UIAlertViewBlocksKitTest.h
@@ -0,0 +1,14 @@
+//
+//  UIAlertViewBlocksKitTest.h
+//  BlocksKit
+//
+//  Created by Zachary Waldowski on 12/20/11.
+//  Copyright (c) 2011 Dizzy Technology. All rights reserved.
+//
+
+#import <GHUnitIOS/GHUnit.h>
+#import "BlocksKit/BlocksKit.h"
+
+@interface UIAlertViewBlocksKitTest : GHTestCase
+
+@end
