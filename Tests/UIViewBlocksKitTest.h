e7b3d8a87f21af03cf66f507574f55cf157d6018
Zachary Waldowski <zwaldowski@gmail.com>
2011-12-20 23:26:29 -0500

    Rather poor UIView unit tests.
    Signed-off-by: Zachary Waldowski <zwaldowski@gmail.com>

---
+++ Tests/UIViewBlocksKitTest.h
@@ -0,0 +1,15 @@
+//
+//  UIViewBlocksKitTest.h
+//  BlocksKit Unit Tests
+//
+
+#import <GHUnitIOS/GHUnit.h>
+#import "BlocksKit/BlocksKit.h"
+
+@interface UIViewBlocksKitTest : GHTestCase
+
+- (void)testOnTouchDown;
+- (void)testOnTouchMove;
+- (void)testOnTouchUp;
+
+@end
