33e6457a4fd5f4a1628ebe73a3714929d23f54eb
Zachary Waldowski <zwaldowski@gmail.com>
2011-12-20 23:36:27 -0500

    UIControl unit tests.
    Signed-off-by: Zachary Waldowski <zwaldowski@gmail.com>

---
+++ Tests/UIControlBlocksKitTest.h
@@ -0,0 +1,11 @@
+//
+//  UIControlBlocksKitTest.h
+//  BlocksKit Unit Tests
+//
+
+#import <GHUnitIOS/GHUnit.h>
+#import "BlocksKit/BlocksKit.h"
+
+@interface UIControlBlocksKitTest : GHTestCase
+
+@end
