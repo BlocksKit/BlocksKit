9e59a55e143ce0a8db4916bd75d5d98e3ce92f2c
Alexsander Akers <a2@pandamonia.us>
2011-12-21 14:31:24 -0500

    Fix potential `NSCacheBlocksKitTest` bug
    Signed-off-by: Alexsander Akers <a2@pandamonia.us>

--- Tests/NSCacheBlocksKitTest.m
+++ Tests/NSCacheBlocksKitTest.m
@@ -61,7 +61,7 @@
     
     [self performSelector:@selector(_succeed) withObject:nil afterDelay:1];
     
-    [self waitForStatus:kGHUnitWaitStatusSuccess timeout:5];
+    [self waitForStatus:kGHUnitWaitStatusSuccess timeout:10];
 }
 
 - (void)_succeed {
@@ -70,6 +70,13 @@
 		NSIndexPath *key = [NSIndexPath indexPathWithIndex:i];
 		if ([_subject objectForKey: key]) count++;
 	}
+	
+	if (_total == OBJECT_COUNT)
+	{
+		[self performSelector:@selector(_succeed) withObject:nil afterDelay:1];
+		return;
+	}
+		
 	GHAssertLessThan(_total, OBJECT_COUNT, @"The cache should have been emptied!");
     GHAssertEquals(_total, count, @"The cache should have been emptied!");
     [self notify:kGHUnitWaitStatusSuccess forSelector:@selector(testEvictionDelegate)];
