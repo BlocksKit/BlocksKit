e1c17f0cf86187c6551c5911d5780022abad25a2
Alexsander Akers <a2@pandamonia.us>
2013-06-29 21:09:39 -0400

    Convert spaces to tabs

--- BlocksKit/MessageUI/MFMessageComposeViewController+BlocksKit.m
+++ BlocksKit/MessageUI/MFMessageComposeViewController+BlocksKit.m
@@ -20,26 +20,27 @@
 	BOOL shouldDismiss = (realDelegate && [realDelegate respondsToSelector:@selector(messageComposeViewController:didFinishWithResult:)]);
 	if (shouldDismiss)
 		[realDelegate messageComposeViewController:controller didFinishWithResult:result];
-	
+
 	void(^block)(MFMessageComposeViewController *, MessageComposeResult) = [self blockImplementationForMethod:_cmd];
-	if (!shouldDismiss)
-	{
-#if __IPHONE_OS_VERSION_MIN_REQUIRED < 60000
-		[controller dismissModalViewControllerAnimated:YES];
-		if (block)
-			block(controller, result);
-#else
-		__weak id wcontroller = controller;
-		[controller dismissViewControllerAnimated:YES completion:^{
-			if (block)
-				block(wcontroller, result);
-		}];
+	if (!shouldDismiss) {
+		if (block) block(controller, result);
+	} else {
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= 60000
+		if ([controller respondsToSelector:@selector(dismissViewControllerAnimated:completion:)]) {
+			__weak typeof(controller) weakController = controller;
+			[controller dismissViewControllerAnimated:YES completion:^{
+				typeof(&*weakController) strongController = weakController;
+				if (block) block(strongController, result);
+			}];
+		} else {
+#endif
+			[controller dismissModalViewControllerAnimated:YES];
+			if (block) block(controller, result);
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= 60000
+		}
 #endif
 	}
-	else if (block)
-	{
-		block(controller, result);
-	}
+>>>>>>> b496d89... Convert spaces to tabs
 }
 
 @end
