a9d41a1cb04317467253d1b34dd80d99bac92551
shaioz <shai.oz@gmail.com>
2013-08-09 23:24:02 +0300

    (for next) changed BKTimerBlock to receive NSTimer instance so you can invalidate the timer within the block. You can still get the interval using [timer timerInterval]

--- BlocksKit/Core/NSTimer+BlocksKit.m
+++ BlocksKit/Core/NSTimer+BlocksKit.m
@@ -13,22 +13,21 @@
 
 @implementation NSTimer (BlocksKit)
 
-+ (id)bk_scheduledTimerWithTimeInterval:(NSTimeInterval)inTimeInterval block:(void (^)(NSTimeInterval time))block repeats:(BOOL)inRepeats
++ (id)bk_scheduledTimerWithTimeInterval:(NSTimeInterval)inTimeInterval block:(void (^)(NSTimer *timer))block repeats:(BOOL)inRepeats
 {
 	NSParameterAssert(block != nil);
 	return [self scheduledTimerWithTimeInterval:inTimeInterval target:self selector:@selector(bk_executeBlockFromTimer:) userInfo:[block copy] repeats:inRepeats];
 }
 
-+ (id)bk_timerWithTimeInterval:(NSTimeInterval)inTimeInterval block:(void (^)(NSTimeInterval time))block repeats:(BOOL)inRepeats
++ (id)bk_timerWithTimeInterval:(NSTimeInterval)inTimeInterval block:(void (^)(NSTimer *timer))block repeats:(BOOL)inRepeats
 {
 	NSParameterAssert(block != nil);
 	return [self timerWithTimeInterval:inTimeInterval target:self selector:@selector(bk_executeBlockFromTimer:) userInfo:[block copy] repeats:inRepeats];
 }
 
 + (void)bk_executeBlockFromTimer:(NSTimer *)aTimer {
-	NSTimeInterval time = [aTimer timeInterval];
-	void (^block)(NSTimeInterval) = [aTimer userInfo];
-	if (block) block(time);
+	void (^block)(NSTimer *) = [aTimer userInfo];
+	if (block) block(aTimer);
 }
 
 @end
