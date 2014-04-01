23e3660ea074f9b3d439dfb3a9f25038a242d612
Maksym Shcheglov <maxim.scheglov@tomtom.com>
2014-04-01 14:51:24 +0200

    Resolved compile time errors in MacDemo

--- Tests/MacDemo/BKApp.m
+++ Tests/MacDemo/BKApp.m
@@ -15,7 +15,7 @@
 #pragma mark - NSArray+BlocksKit
 
 	// We "reduce" the colorList's keys (x) into the "sum" array, which is ininitally empty. (@[])
-	NSArray *cryns = [clrLst.allKeys reduce:@[] withBlock:^id(NSArray *sum, id x) {
+	NSArray *cryns = [clrLst.allKeys bk_reduce:@[] withBlock:^id(NSArray *sum, id x) {
 		// Return the accumulator "sum" array that gets one more name/NSColor dictionary on each pass.
 		return [sum arrayByAddingObject:@{@"name":x, @"color":[clrLst colorWithKey:x]}];
 	}];
@@ -24,21 +24,21 @@
 
 	// Use BK to easily "Associated Values" to any object.
 	// Here, we save the array we just made to "self", for use later. No property/ivar necessary!
-	[self associateValue:cryns withKey:(const void*)@"assocCryns"];
+	[self bk_associateValue:cryns withKey:(const void*)@"assocCryns"];
 
 #pragma mark - NSTimer+BlocksKit
 
 	// timer w/ block will bump our displayCount property and repeat itself, triggering KVO notification below
-	_tmr = [NSTimer scheduledTimerWithTimeInterval:.4 block:^(NSTimer *t) { self.displayCt++; } repeats:YES];
+	_tmr = [NSTimer bk_scheduledTimerWithTimeInterval:.4 block:^(NSTimer *t) { self.displayCt++; } repeats:YES];
 
 #pragma mark - NSObject+BlockObservation
 
 	// A simpler, blocks based alternative to "observeValueForKeyPath:ofObject:change:context:"
 	// Thanks to KVO, this block "task" will get called every time our "displayCount" property changes (ie. via the timer)
-	[self addObserverForKeyPath:@"displayCt" options:2 task:^(BKApp*obj,NSDictionary*change) {
+	[self bk_addObserverForKeyPath:@"displayCt" options:2 task:^(BKApp*obj,NSDictionary*change) {
 
 		// reload table and scroll to the bottom of the list when displayCt changes
-		obj.displayCt != [[obj associatedValueForKey:@"assocCryns"]count]
+		obj.displayCt != [[obj bk_associatedValueForKey:@"assocCryns"]count]
 		? 	[obj.tv  reloadData], [obj.tv.enclosingScrollView.documentView scrollPoint:NSMakePoint(0,10000)]
 		: 	[obj.tmr invalidate]; // if displayCt == count of dicts in "crayon" array, "invalidate" timer
 	}];
@@ -46,7 +46,7 @@
 #pragma mark - Dynamic DataSource
 
 	// assign/instantiate A2DynamicDelegate from a tableView's dynamicDataSource property (via BlocksKit's magic!)
-	_ddSrc = _tv.dynamicDataSource;
+	_ddSrc = _tv.bk_dynamicDataSource;
 
 	// The 2 required <NSTableViewDataSource> methods we will dynamically "delegate".
 	SEL numberOfRowsMethod 			 = @selector(numberOfRowsInTableView:),
@@ -56,7 +56,7 @@
 	NSInteger  (^numberOfRowsBlock)(NSTableView*) 								  = ^NSInteger(NSTableView*tv){ return _displayCt; };
 	id (^objectValueForColRowBlock)(NSTableView*,NSTableColumn*,NSInteger) = ^id(NSTableView*tv,NSTableColumn*tc,NSInteger row) {
 		// Here we access the array of dictionaries we saved to "self", returning the appropriate "objectValueForTableColumn:row:"
-		return [self associatedValueForKey:@"assocCryns"][row][[tv.tableColumns indexOfObject:tc] ? @"color" : @"name"];
+		return [self bk_associatedValueForKey:@"assocCryns"][row][[tv.tableColumns indexOfObject:tc] ? @"color" : @"name"];
 	};
 	// Tell A2DynamicDelegate to implement the datasource method with the block we just defined. (these populate the table!).
 	[_ddSrc implementMethod:numberOfRowsMethod 			withBlock:			numberOfRowsBlock];
