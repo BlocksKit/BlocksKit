7825fdde754f2c59d2393024dbd9ae712ea714ff
zhengxin <zx19880528@qq.com>
2015-04-07 14:06:08 +0800

    modified:   BlocksKit/UIKit/UITextField+BlocksKit.h     add some convenient param name

--- BlocksKit/UIKit/UITextField+BlocksKit.h
+++ BlocksKit/UIKit/UITextField+BlocksKit.h
@@ -22,44 +22,44 @@
  *
  *  The return value indicates whether the receiver should begin editing
  */
-@property (nonatomic, copy) BOOL(^bk_shouldBeginEditingBlock)(UITextField *);
+@property (nonatomic, copy) BOOL(^bk_shouldBeginEditingBlock)(UITextField *textField);
 
 /**
  *	The block that fires after the receiver begins editing
  */
-@property (nonatomic, copy) void(^bk_didBeginEditingBlock)(UITextField *);
+@property (nonatomic, copy) void(^bk_didBeginEditingBlock)(UITextField *textField);
 
 /**
  *	The block that fires before the receiver ends editing
  *
  *  The return value indicates whether the receiver should end editing
  */
-@property (nonatomic, copy) BOOL(^bk_shouldEndEditingBlock)(UITextField *);
+@property (nonatomic, copy) BOOL(^bk_shouldEndEditingBlock)(UITextField *textField);
 
 /**
  *	The block that fires after the receiver ends editing
  */
-@property (nonatomic, copy) void(^bk_didEndEditingBlock)(UITextField *);
+@property (nonatomic, copy) void(^bk_didEndEditingBlock)(UITextField *textField);
 
 /**
  *	The block that fires when the receiver's text will change
  *
  *  The return value indicates whether the receiver should replace the characters in the given range with the replacement string
  */
-@property (nonatomic, copy) BOOL(^bk_shouldChangeCharactersInRangeWithReplacementStringBlock)(UITextField *, NSRange, NSString *);
+@property (nonatomic, copy) BOOL(^bk_shouldChangeCharactersInRangeWithReplacementStringBlock)(UITextField *textField, NSRange range, NSString *string);
 
 /**
  *	The block that fires when the receiver's clear button is pressed
  *
  *  The return value indicates whether the receiver should clear its contents
  */
-@property (nonatomic, copy) BOOL(^bk_shouldClearBlock)(UITextField *);
+@property (nonatomic, copy) BOOL(^bk_shouldClearBlock)(UITextField *textField);
 
 /**
  *	The block that fires when the keyboard's return button is pressed and the receiver is the first responder
  *
  *  The return value indicates whether the receiver should return
  */
-@property (nonatomic, copy) BOOL(^bk_shouldReturnBlock)(UITextField *);
+@property (nonatomic, copy) BOOL(^bk_shouldReturnBlock)(UITextField *textField);
 
 @end
